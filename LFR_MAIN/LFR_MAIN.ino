28e42d124fef2c09e358ac0ecfad325c21760f1f
Pravin-me <pravinpandeypra@gmail.com>
2026-07-18 13:56:58 +0545

    Update LFR_MAIN.ino

--- LFR_MAIN/LFR_MAIN.ino
+++ LFR_MAIN/LFR_MAIN.ino
@@ -12,15 +12,15 @@ int control_right2=4;
 int control_left1=7;
 int control_left2=9;
 int standby=8;
-int base_speed=155;
-int motor_pid_speed;
+int base_speed=200;
+int motor_pid_speed=0;
 //Equation PART
 int p,i,d;
 int error=0,last_error=0;
 int pterm,iterm,dterm;
 //Sensor PART
-int calibrated[8];
-long int sum[]={0,0,0,0,0,0,0,0};
+int calibrated[6];
+long int sum[]={0,0,0,0,0,0};
 
 
 void setup(){
@@ -84,13 +84,14 @@ void motordriving(){
   motor_speed_left = base_speed - motor_pid_speed;
 }
 void move(){
+  motordriving();
   digitalWrite(standby,HIGH);
   digitalWrite(control_left1,HIGH);
   digitalWrite(control_left2,LOW);
   digitalWrite(control_right1,HIGH);
   digitalWrite(control_right2,LOW);
   analogWrite(motor_outpin_right,motor_speed_right);
-  analogWrite(motor_outpin_right,motor_speed_left);
+  analogWrite(motor_outpin_left,motor_speed_left);
 }
 void stall(){
   digitalWrite(standby,LOW);
