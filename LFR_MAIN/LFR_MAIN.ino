39d65917880f3c51d08f290efdff40edcb41a7f9
Pravin-me <pravinpandeypra@gmail.com>
2026-07-22 20:47:20 +0545

    Update LFR_MAIN.ino

--- LFR_MAIN/LFR_MAIN.ino
+++ LFR_MAIN/LFR_MAIN.ino
@@ -1,7 +1,5 @@
-#include <QTRSensors.h>
-
-QTRSensors qtr;
 uint16_t sensorValues[6];
+int button = 13;
 //MOTOR PART
 int motor_speed_right;
 int motor_speed_left;
@@ -12,11 +10,11 @@ int control_right2=4;
 int control_left1=7;
 int control_left2=9;
 int standby=8;
-int base_speed=200;
+int base_speed=175;
 int motor_pid_speed=0;
 //Equation PART
-int p,i,d;
-int error=0,last_error=0;
+int p=50,i=0,d=0;
+float error=0,last_error=0;
 int pterm,iterm,dterm;
 //Sensor PART
 int calibrated[6];
@@ -24,10 +22,13 @@ long int sum[]={0,0,0,0,0,0};
 
 
 void setup(){
-  qtr.setTypeAnalog();
-  qtr.setSensorPins((const uint8_t[]){A0,A1,A2,A3,A4,A5}, 6);
-  qtr.setEmitterPin(2);
-
+  //Inputs for sensor
+  pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
+  pinMode(A2,INPUT);
+  pinMode(A3,INPUT);
+  pinMode(A4,INPUT);
+  pinMode(A5,INPUT);
   //analogWriteResolution()
 
   Serial.begin(9600);
@@ -38,50 +39,92 @@ void setup(){
   pinMode(standby,OUTPUT);  
   pinMode(motor_outpin_right,OUTPUT);
   pinMode(motor_outpin_left,OUTPUT);
+  pinMode(button,INPUT_PULLUP);
+  while(digitalRead(button));
   calibrate();
+  while(digitalRead(button));
 }
 
 void loop(){
-  // fetch_sensors();
-  // check_output();
-move();
-delay(2000);
-stall();
-delay(2000);
+  fetch_sensors();
+  refresh_PID();
+  //move();
+  check_output();
 } 
 
 void fetch_sensors(){
-  qtr.read(sensorValues);
+  sensorValues[0] = analogRead(A0);
+  sensorValues[1] = analogRead(A1);
+  sensorValues[2] = analogRead(A2);
+  sensorValues[3] = analogRead(A3);
+  sensorValues[4] = analogRead(A4);
+  sensorValues[5] = analogRead(A5);
   for(int i=0;i<6;i++){
     calibrated[i] = sensorValues[i]-sum[i];
   }
 }
 
 void check_output(){
+  calc_error();
   for(int i=0;i<6;i++){
     Serial.print(calibrated[i]);
     Serial.print("\t");
   }
   Serial.println();
+  Serial.print(error);
+  Serial.println();
   delay(250);
 }
 
 void calibrate(){
-  for(int j=1;j<=500;j++){
-  qtr.read(sensorValues);
+  fetch_sensors();
+  //digitalWrite(LED,LOW);
+  delay(1000);
+  //digitalWrite(LED,HIGH);
+  for(int j=1;j<=2000;j++){
+
   for(int i=0;i<6;i++){
     sum[i]+=sensorValues[i];
   }
   }
   for(int i=0;i<6;i++){
-    sum[i]/=500;
+    sum[i]/=2000;
   }
-  delay(2000);
+  // digitalWrite(LED,LOW);
+  // delay(200);
+  // digitalWrite(LED,HIGH);
+  // delay(200);
+  // digitalWrite(LED,LOW);
+  // delay(200);
+  // digitalWrite(LED,HIGH);
+}
+void new_calibration(){
+  int highest[] = {0,0,0,0,0},lowest[]= {0,0,0,0,0};
+  digitalWrite(standby,HIGH);
+  digitalWrite(control_left1,LOW);
+  digitalWrite(control_left2,HIGH);
+  digitalWrite(control_right1,HIGH);
+  digitalWrite(control_right2,LOW);
+  analogWrite(motor_outpin_right,220);
+  analogWrite(motor_outpin_left,220);
+  for(int i=0;i<2000;i++){
+    fetch_sensors();
+    if(sensorValues[i]>highest[i]){
+      highest[i] = sensorValues[i];
+    }
+    else if(sensorValues[i]<lowest[i]){
+      lowest[i] = sensorValues[i];
+    }
+  }
+  for(int i=0;i<5;i++){
+    calibrated[i] = (highest[i]+lowest[i])/2;
+  }
+
 }
 
 void motordriving(){
-  motor_speed_right = base_speed + motor_pid_speed;
   motor_speed_left = base_speed - motor_pid_speed;
+  motor_speed_right = base_speed + motor_pid_speed;
 }
 void move(){
   motordriving();
@@ -97,10 +140,17 @@ void stall(){
   digitalWrite(standby,LOW);
 }
 void refresh_PID(){
+  calc_error();
   pterm = error;
   iterm += error;
   dterm = error - last_error;
   motor_pid_speed = (p*pterm)+(i*iterm)+(d*dterm);
-  //FOR TESTING
-  motor_pid_speed = 0;
+  last_error = error;
+}
+void calc_error(){
+ //FOR 6 sensors active
+ // error = sensorValues[2]-sensorValues[3]+2*(sensorValues[1]-sensorValues[4])+4*(sensorValues[0]-sensorValues[5]);
+ //FOR 5 sensors active
+ error = sensorValues[3]+2*(sensorValues[4]-sensorValues[2])+4*(sensorValues[0]-sensorValues[0]);
+ error = error/10;
 }
